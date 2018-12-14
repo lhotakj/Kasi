b9dfc99a065676bcbd7099181a8ada1c4cdbcd1d
Jaroslav Lhotak <lhotaj@gmail.com>
2018-12-14 13:23:36 +0100

    paths

--- test/start.sh
+++ test/start.sh
@@ -6,7 +6,7 @@
 # eg. start.sh /usr/bin/python3
 
 
-if [[ "$1" == "" ]]; then
+if [ "$1" == "" ]; then
     echo "No interpreter supplied. Usage: 'start.sh /usr/bin/python3'"
     exit 1
 fi
@@ -18,7 +18,7 @@ nohup $1 ./start.py &
 PID=$!
 echo "Server running under PID:$PID"
 echo "<here comes the test> ..."
-$1 ./client.py
+$1 ~/kasi/test/client.py
 
 echo "Killing server $PID ..."
 kill -9 $PID
