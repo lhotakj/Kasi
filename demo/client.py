c620f72dabe7ec7d9cbbb69d1e125cad3c6e3a1d
Jaroslav Lhoták <lhotakj@gmail.com>
2019-01-09 23:03:20 +0100

    Update client.py

--- demo/client.py
+++ demo/client.py
@@ -4,31 +4,33 @@
 from kasi import Client
 from datetime import datetime, timedelta
 
-
-
 if __name__ == '__main__':
 
-
+    # create an instance of client
     client = Client.Client(host='localhost', port=5000)
-
     client.reset()
-
-    client.set("D1", "Nonssssssssse", domain="X")
+    
+    # save string into domain X
+    client.set("D1", "test1", domain="X")
     print(str(client.get("D1", domain="X")))
 
-    client.set("list", ['a', 'd'], timedelta(seconds=1))
-    client.set("text", 'AHOJ', timedelta(hours=1), domain="default")
-    client.set("unicode1", u'2H₂ + O₂ ⇌ 2H₂O, R = 4.7 kΩ, ⌀ 200 mm')
-    client.set("unicode2", u'ก ข ฃ ค ฅ ฆ ง จ ฉ ช ซ ฌ ญ ฎ ฏ ฐ ฑ ฒ ณ ด ต ถ ท ธ น บ ป ผ ฝ พ ฟ ภ ม ย ร ฤ ล ฦ ว ศ ษ ส ห ฬ อ ฮ ฯ ะ ั า ำ ิ ี ึ ื ุ ู ฺ ฿ เ แ โ ใ ไ ๅ ๆ ็ ่ ้ ๊ ๋ ์ ํ ๎ ๏ ๐ ๑ ๒ ๓ ๔ ๕ ๖ ๗ ๘ ๙ ๚ ๛')
+    # save non-trivial object
+    client.set("list", ['a', 'd'], timedelta(seconds=1))               # set expiration 1 second
+    client.set("text", 'hello', timedelta(hours=1), domain="default")  # set expiration 1 hour and save into domain 'default'
+    client.set("unicode1", u'2H₂ + O₂ ⇌ 2H₂O, R = 4.7 kΩ, ⌀ 200 mm')   # store unicode
+    client.set("unicode2", u'ก ข ฃ ค ฅ ฆ ง จ ฉ ช ซ ฌ ญ ฎ ฏ ')        # store unicode
 
+    # get them
     print(str(client.get("list")))
+    print(str(client.get("text")))
     print(str(client.get("unicode1")))
     print(str(client.get("unicode2")))
-    print(str(client.get("hovni")))
+    print(str(client.get("not-existing")))    # demonstrate reading non-existing key
 
     client.delete("D1", domain="X")
     print(str(client.stats()))
 
+    # some performance
     #    print("==PERF==")
     #    start = datetime.now().timestamp()
     #    for x in range(0,1000):
